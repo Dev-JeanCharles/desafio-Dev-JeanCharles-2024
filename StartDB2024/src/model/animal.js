20df5eea197528702f0ccb58a7b42706e91e401a
Dev-JeanCharles <jeancduasan@gmail.com>
2024-09-06 11:58:32 -0300

    :sparkles: feat: classes recinto e animal

---
+++ StartDB2024/src/model/animal.js
@@ -0,0 +1,8 @@
+class Animal {
+
+  constructor(especie, tamanho, biomas) {
+    this.especie = especie;
+    this.tamanho = tamanho;
+    this.biomas = biomas
+  }
+}
---
+++ StartDB2024/src/model/recinto.js
@@ -0,0 +1,10 @@
+class Recinto {
+
+  constructor(numero, biomas, tamanhoTotal) {
+    this.numero = numero;
+    this.biomas = biomas;
+    this.tamanhoTotal = tamanhoTotal;
+    this.animais = [];
+    this.espacoEmUso = 0;
+  }
+}
